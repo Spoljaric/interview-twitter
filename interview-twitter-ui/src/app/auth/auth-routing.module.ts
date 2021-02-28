2e8d8c1661266d408d6129a451670dbc33d02aa5
Borna Spoljaric <bspoljaric@croz.net>
2021-02-28 10:27:23 +0100

    add register base

--- interview-twitter-ui/src/app/auth/auth-routing.module.ts
+++ interview-twitter-ui/src/app/auth/auth-routing.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginContainerComponent} from "./login-container/login-container.component";
+import {RegisterComponent} from './register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginContainerComponent,
   },
+  {path: 'register', component: RegisterComponent},
 ];
 
 @NgModule({
